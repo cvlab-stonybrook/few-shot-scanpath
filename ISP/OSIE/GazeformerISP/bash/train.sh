ac68dd446a478de2a905eb77f8582a5082655daf
RuoyuX-2018 <58375155+RuoyuX-2018@users.noreply.github.com>
2025-06-04 21:50:33 -0400

    Update train.sh

--- ISP/OSIE/GazeformerISP/bash/train.sh
+++ ISP/OSIE/GazeformerISP/bash/train.sh
@@ -2,14 +2,14 @@
 DATASET_NAME='OSIE'
 MODEL_NAME='baseline'
 
-CUDA_VISIBLE_DEVICES=5 python src/train.py \
-  --log_root /data/add_disk4/ruoyu/project/ISP/assets/git-osie-useremb-ex-10to15 \
+CUDA_VISIBLE_DEVICES=0 python src/train.py \
+  --log_root assets/OSIE-ex-10to15 \
   --subject_feature_dim 384 \
-  --batch 4 \
+  --batch 2 \
   --epoch 40 \
   --no_eval_epoch 30 \
   --ex_subject 10 11 12 13 14 \
   --subject_num 10 \
-  --user_emb_path /data/add_disk4/ruoyu/project/phat/git-osie-useremb-ex-10to15/train_user_embedding.pt
+  --user_emb_path ../../../SE-Net/assets/OSIE-ex-10to15/train_user_embedding.pt
 
 
